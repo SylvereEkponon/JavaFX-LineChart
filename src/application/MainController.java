984750e9105e45294714dfb812919779d10faa88
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-11-14 11:23:37 -0500

    Added Multiple lines to LineChart

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -21,8 +21,39 @@ public class MainController {
 		series.getData().add(new XYChart.Data<String, Number>("May",300));
 		series.getData().add(new XYChart.Data<String, Number>("Jun",550));
 		series.getData().add(new XYChart.Data<String, Number>("Jul",600));
-		series.setName("Month Pay");
-		lineChart.getData().add(series);
+		series.setName("Pay 1");
+		
+		XYChart.Series<String, Number> series1 = new XYChart.Series<String, Number>();
+		series1.getData().add(new XYChart.Data<String, Number>("Jan",240));
+		series1.getData().add(new XYChart.Data<String, Number>("Feb",250));
+		series1.getData().add(new XYChart.Data<String, Number>("Mar",220));
+		series1.getData().add(new XYChart.Data<String, Number>("Apr",300));
+		series1.getData().add(new XYChart.Data<String, Number>("May",400));
+		series1.getData().add(new XYChart.Data<String, Number>("Jun",550));
+		series1.getData().add(new XYChart.Data<String, Number>("Jul",575));
+		series1.setName("Pay 2");
+		
+		XYChart.Series<String, Number> series2 = new XYChart.Series<String, Number>();
+		series2.getData().add(new XYChart.Data<String, Number>("Jan",400));
+		series2.getData().add(new XYChart.Data<String, Number>("Feb",300));
+		series2.getData().add(new XYChart.Data<String, Number>("Mar",220));
+		series2.getData().add(new XYChart.Data<String, Number>("Apr",300));
+		series2.getData().add(new XYChart.Data<String, Number>("May",300));
+		series2.getData().add(new XYChart.Data<String, Number>("Jun",520));
+		series2.getData().add(new XYChart.Data<String, Number>("Jul",500));
+		series2.setName("Pay 3");
+		
+		XYChart.Series<String, Number> series3 = new XYChart.Series<String, Number>();
+		series3.getData().add(new XYChart.Data<String, Number>("Jan",300));
+		series3.getData().add(new XYChart.Data<String, Number>("Feb",285));
+		series3.getData().add(new XYChart.Data<String, Number>("Mar",400));
+		series3.getData().add(new XYChart.Data<String, Number>("Apr",450));
+		series3.getData().add(new XYChart.Data<String, Number>("May",400));
+		series3.getData().add(new XYChart.Data<String, Number>("Jun",550));
+		series3.getData().add(new XYChart.Data<String, Number>("Jul",500));
+		series3.setName("Pay 4");
+		
+		lineChart.getData().addAll(series,series1,series2,series3);
 	}
 
 }
